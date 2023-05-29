de7a46aadf262e4d6175270b7982b4d18e6b5688
jord1990 <jordy.hoogeboom@hotmail.nl>
2023-05-29 20:18:19 +0200

    fished the task

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <>
+            <h1>Home</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda, facere illum modi non ullam vero vitae. Aperiam assumenda consectetur tenetur. Ab aperiam commodi cupiditate distinctio, ea eaque explicabo in inventore itaque iusto labore, laboriosam libero modi nam nemo, odit placeat porro quam quia quo reiciendis repellendus sit. Accusamus cumque maxime odio officiis rerum saepe sint voluptate. Accusantium eos explicabo fugit magnam, nihil quaerat ullam voluptates. Aliquam at blanditiis cumque dicta dignissimos dolor eius esse est illo illum ipsa ipsam iusto libero minima molestiae necessitatibus nisi non quisquam quo recusandae sed soluta, sunt suscipit tempora velit veritatis voluptatibus. Ipsa, nam.</p>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/pages/login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Login({ updateStatus }) {
+
+
+
+    return (
+        <>
+            <button onClick={updateStatus}>Login</button>
+        </>
+    );
+}
+
+export default Login;
